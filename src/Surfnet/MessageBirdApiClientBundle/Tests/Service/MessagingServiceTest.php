c6ae8e3cef5e762238ea3b0c3a7d2416fcea35df
Reinier Kip <reinier@ibuildings.nl>
2014-09-18 13:21:51 +0200

    Add Mockery listener and fix bundle MessagingService tests.

--- src/Surfnet/MessageBirdApiClientBundle/Tests/Service/MessagingServiceTest.php
+++ src/Surfnet/MessageBirdApiClientBundle/Tests/Service/MessagingServiceTest.php
@@ -44,7 +44,7 @@ class MessagingServiceTest extends \PHPUnit_Framework_TestCase
     public function testItLogsInvalidMessages()
     {
         $message = new Message('31612345678', 'body');
-        $result = new SendMessageResult(SendMessageResult::STATUS_NOT_SENT, ['code' => SendMessageResult::ERROR_INVALID_PARAMS]);
+        $result = new SendMessageResult(SendMessageResult::STATUS_NOT_SENT, [['code' => SendMessageResult::ERROR_INVALID_PARAMS, 'description' => '']]);
 
         $libraryService = m::mock('Surfnet\MessageBirdApiClient\Messaging\MessagingService')
             ->shouldReceive('send')->once()->with($message)->andReturn($result)
@@ -60,7 +60,7 @@ class MessagingServiceTest extends \PHPUnit_Framework_TestCase
     public function testItLogsInvalidAccessKeys()
     {
         $message = new Message('31612345678', 'body');
-        $result = new SendMessageResult(SendMessageResult::STATUS_NOT_SENT, ['code' => SendMessageResult::ERROR_INVALID_PARAMS]);
+        $result = new SendMessageResult(SendMessageResult::STATUS_NOT_SENT, [['code' => SendMessageResult::ERROR_REQUEST_NOT_ALLOWED, 'description' => '']]);
 
         $libraryService = m::mock('Surfnet\MessageBirdApiClient\Messaging\MessagingService')
             ->shouldReceive('send')->once()->with($message)->andReturn($result)
